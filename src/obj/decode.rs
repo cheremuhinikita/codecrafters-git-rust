11911fcb1423b249a24fb9856ea17332abc4c3f2
cheremuhinikita <nikitadf68@gmail.com>
2022-11-27 22:03:15 +0300

    refactor: decode git object

--- src/obj/decode.rs
+++ src/obj/decode.rs
@@ -7,8 +7,8 @@ pub fn decode(input: &[u8]) -> Result<Object> {
     let input = str::from_utf8(input)?;
     let (input, raw) = parse_raw_object(input).map_err(|e| Error::ParseObject(e.to_string()))?;
 
-    if input != "" {
-        return Err(Error::ParseObject(String::from("input not complete")));
+    if !input.is_empty() {
+        Err(Error::ParseObject(String::from("input not complete")))
     } else {
         Object::from_raw(raw)
     }
