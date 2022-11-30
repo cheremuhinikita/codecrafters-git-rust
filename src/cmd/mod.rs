5174444cb25b2f3c39de0f822fb86859b3b8948d
cheremuhinikita <nikitadf68@gmail.com>
2022-11-30 23:46:37 +0300

    fix: add commit tree to command

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -7,7 +7,8 @@ pub mod ls_tree;
 pub mod write_tree;
 
 use self::{
-    cat_file::CatFile, hash_object::HashObject, init::Init, ls_tree::LsTree, write_tree::WriteTree,
+    cat_file::CatFile, commit_tree::CommitTree, hash_object::HashObject, init::Init,
+    ls_tree::LsTree, write_tree::WriteTree,
 };
 use crate::error::{Error, Result};
 
@@ -17,6 +18,7 @@ pub enum Command {
     HashObject(HashObject),
     LsTree(LsTree),
     WriteTree(WriteTree),
+    CommitTree(CommitTree),
 }
 
 impl Command {
@@ -32,6 +34,7 @@ impl Command {
             "hash-object" => Self::HashObject(HashObject::parse(args)?),
             "ls-tree" => Self::LsTree(LsTree::parse(args)?),
             "write-tree" => Self::WriteTree(WriteTree),
+            "commit-tree" => Self::CommitTree(CommitTree::parse(args)?),
             _ => return Err(Error::ParseCommand(format!("unknown command: {}", command))),
         };
 
@@ -45,6 +48,7 @@ impl Command {
             Self::HashObject(hash_object) => hash_object.exec(),
             Self::LsTree(ls_tree) => ls_tree.exec(),
             Self::WriteTree(write_tree) => write_tree.exec(),
+            Self::CommitTree(commit_tree) => commit_tree.exec(),
         }
     }
 }
